d77954cb84b5c31bcc65154686ccb48ac489be84
Felipe Llerena Catalan <felipellc7@gmail.com>
2023-11-19 17:02:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Juego lúdico en señas</h1>
+      <div style={{ width: "90vw" }}>
+        <iframe
+          allowfullscreen
+          style={{ maxWidth: "100%" }}
+          src="https://wordwall.net/es/embed/9f8c3170d79a406c8c26adc4b5a7fad0?themeId=1&templateId=3&fontStackId=0"
+          width="1200"
+          height="700"
+          frameborder="0"
+          title="game"
+        ></iframe>
+      </div>
     </div>
   );
 }
